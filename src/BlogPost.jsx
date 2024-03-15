e83c8c8a5167c4e239c3358702f904eecc5ae9de
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-03-15 17:21:32 -0500

    use navigate hook

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,15 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./blogdata";
 
 export const BlogPost = () => {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const blogpost = blogdata.find((post) => post.slug == slug);
+  const regresarAPaginaAnterior = () => {
+    navigate("/blog"); // Para siempre ser dirigidos a la página de blog
+    // navigate(-1); // Para siempre ser dirigidos a la página de home
+  };
   return (
     <>
       <h2>Blog Post</h2>
       <hr />
       <h5>{blogpost.title}</h5>
       <p>{blogpost.content}</p>
+      <button onClick={() => regresarAPaginaAnterior()}>Volver atras</button>
     </>
   );
 };
