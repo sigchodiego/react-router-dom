88b653ac0b25c3a993f3d209ed3e077ed7856329
Diego Sigcho Alvarado <sigchodiego@gmail.com>
2024-03-15 17:35:47 -0500

    Outlet hook

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ function App() {
         <Menu />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/blog" element={<BlogPage />}>
+            <Route path=":slug" element={<BlogPost />} />
+          </Route>
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/*" element={<h1>Not found</h1> /* Default page */} />
         </Routes>
--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { blogdata } from "./blogdata.js";
 
 export const BlogPage = () => {
   return (
     <>
       <p>Blog Page</p>
+      <Outlet />
       <ul>
         {blogdata.map((post) => (
           <BlockLink key={post.text} post={post} />
